0bd931f1bbb47b1525c2100a2451ec76026dfe1c
Charlie Duso <dusocharles@hotmail.com>
2016-03-01 15:24:51 -0700

    more deletions

--- SW_Flow_lib.c
+++ SW_Flow_lib.c
@@ -102,11 +102,9 @@
 /*                  Global Variables                   */
 /* --------------------------------------------------- */
 extern SW_SITE SW_Site;
-extern SW_SOILWAT SW_Soilwat;
 unsigned int soil_temp_error;  // simply keeps track of whether or not an error has been reported in the soil_temperature function.  0 for no, 1 for yes.
 unsigned int soil_temp_init;   // simply keeps track of whether or not the values for the soil_temperature function have been initialized.  0 for no, 1 for yes.
 unsigned int fusion_pool_init;   // simply keeps track of whether or not the values for the soil fusion (thawing/freezing) section of the soil_temperature function have been initialized.  0 for no, 1 for yes.
-unsigned int count;
 
 /* *************************************************** */
 /*                Module-Level Variables               */
