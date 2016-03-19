59299881b8216ddcc544e67be54f848766cae590
[CaitlinA] <[candrews@usgs.gov]>
2016-03-18 17:00:57 -0700

    Changed how biomass is calculated for surface temp equation

--- SW_Flow.c
+++ SW_Flow.c
@@ -663,8 +663,8 @@ void SW_Water_Flow(void) {
 	/* Soil Temperature starts here */
 
 	double biomass; // computing the live biomass real quickly to condense the call to soil_temperature
-biomass = SW_VegProd.grass.biolive_daily[doy] * SW_VegProd.fractionGrass + SW_VegProd.shrub.biolive_daily[doy] * SW_VegProd.fractionShrub
-		+ SW_VegProd.forb.biolive_daily[doy] * SW_VegProd.fractionForb + SW_VegProd.tree.biolive_daily[doy] * SW_VegProd.fractionTree; // changed to exclude tree biomass, bMatric/c it was breaking the soil_temperature function
+biomass = SW_VegProd.grass.biomass_daily[doy] * SW_VegProd.fractionGrass + SW_VegProd.shrub.biolive_daily[doy] * SW_VegProd.fractionShrub
+		+ SW_VegProd.forb.biomass_daily[doy] * SW_VegProd.fractionForb + SW_VegProd.tree.biolive_daily[doy] * SW_VegProd.fractionTree; // changed to exclude tree biomass, bMatric/c it was breaking the soil_temperature function
 
 			// soil_temperature function computes the soil temp for each layer and stores it in lyrsTemp
 			// doesn't affect SWC at all, but needs it for the calculation, so therefore the temperature is the last calculation done
