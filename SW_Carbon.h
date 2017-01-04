a15fb5e8327c0b487665e63ab2a6a118b596e16b
Charlie Duso <dusocharles@hotmail.com>
2017-01-04 15:09:45 -0700

    Setup multiplier calculation functions.

--- SW_Carbon.h
+++ SW_Carbon.h
@@ -1,14 +1,29 @@
 /**
  * @file   SW_Carbon.h
  * @author Zachary Kramer, Charles Duso
- * @brief  Contains functions, constants, and variables that deal with the effect of
- *         CO2 on transpiration and biomass.
+ * @brief  Contains functions, constants, and variables that deal with the
+ *         effect of CO2 on transpiration and biomass.
  *
  * @data   04 January 2016
  */
 
 
 /* Global Variables - Must include this header file to use these values */
-extern double       co2_biomass_mult; // Biomass multiplier
-extern double       co2_wue_mult;     // WUE multiplier
-extern unsigned int calculate_co2;    // Logical to determine if co2 is used
+extern double       co2_biomass_mult;   // Biomass multiplier
+extern double       co2_wue_mult;       // WUE multiplier
+extern unsigned int calculate_co2;      // Logical to determine if co2 is used
+
+/* Function Prototypes */
+// TODO
+static inline void calc_CO2_Transp();   // Calculate transpiration coefficient
+// TODO
+static inline void calc_CO2_Biomass();  // Calculate biomass coeefficient
+
+/* Function Declarations */
+static inline void calc_CO2_Transp() {
+  // TODO
+}
+
+static inline void calc_CO2_Biomass() {
+  // TODO
+}
