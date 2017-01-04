248854afe2f8edb0a362528a1bae49ccf2431c67
Charlie Duso <dusocharles@hotmail.com>
2017-01-04 13:06:18 -0700

    Added new header file that controls CO2 impacts.
    This header will contain functions, constants, and variables that will
    handle CO2 impacts on transpiration and biomass overtime.

---
+++ SW_Carbon.h
@@ -0,0 +1,14 @@
+/**
+ * @file   SW_Carbon.h
+ * @author Zachary Kramer, Charles Duso
+ * @brief  Contains functions, constants, and variables that deal with the effect of
+ *         CO2 on transpiration and biomass.
+ *
+ * @data   04 January 2016
+ */
+
+
+/* Global Variables - Must include this header file to use these values */
+extern double       co2_biomass_mult; // Biomass multiplier
+extern double       co2_wue_mult;     // WUE multiplier
+extern unsigned int curr_year;        // Variable to track the current year
