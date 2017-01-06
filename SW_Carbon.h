66b21dfab7ffe54440dc66e54876bdd7714930e3
Charlie Duso <dusocharles@hotmail.com>
2017-01-06 12:43:18 -0700

    Modified multiplier variables so that they are compatible with R.

--- SW_Carbon.h
+++ SW_Carbon.h
@@ -4,26 +4,16 @@
  * @brief  Contains functions, constants, and variables that deal with the
  *         effect of CO2 on transpiration and biomass.
  *
- * @data   04 January 2016
+ * @data   06 January 2016
  */
 
+#ifdef RSOILWAT
 
 /* Global Variables - Must include this header file to use these values */
-extern double       co2_biomass_mult;   // Biomass multiplier
-extern double       co2_wue_mult;       // WUE multiplier
+extern RealD        co2_biomass_mult;   // Biomass multiplier
+extern RealD        co2_wue_mult;       // WUE multiplier
+extern SEXP         co2_multipliers;    // R object containing CO2 multipliers
+
+#endif
+
 extern unsigned int calculate_co2;      // Logical to determine if co2 is used
-
-/* Function Prototypes */
-// TODO
-static inline void calc_CO2_Transp();   // Calculate transpiration coefficient
-// TODO
-static inline void calc_CO2_Biomass();  // Calculate biomass coeefficient
-
-/* Function Declarations */
-static inline void calc_CO2_Transp() {
-  // TODO
-}
-
-static inline void calc_CO2_Biomass() {
-  // TODO
-}
