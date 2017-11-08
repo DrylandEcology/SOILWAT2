2836f729b6effe6a89303761c0bf5e0cdfe7c41b
candrews@usgs.gov <candrews@usgs.gov>
2017-11-08 14:52:10 -0700

    Unit test of beta random function

--- test/sw_test.cc
+++ test/sw_test.cc
@@ -1,6 +1,22 @@
 #include "gtest/gtest.h"
+#include "../rands.h"
 
 namespace {
+  // This test belongs to the beta random number generator
+  TEST(BetaGenerator, ZeroToOneOutput) {
+    EXPECT_LT(genbet(0.5, 2), 1);
+    EXPECT_LT(genbet(1, 3), 1);
+    EXPECT_GT(genbet(1, 4), 0);
+    EXPECT_GT(genbet(0.25, 1), 0);
+
+  }
+
+  TEST(BetaGenerator, Errors) {
+    EXPECT_ANY_THROW(genbet(-0.5, 2));
+    EXPECT_ANY_THROW(genbet(1, -3));
+    EXPECT_ANY_THROW(genbet(-1, -3));
+  }
+
 
 TEST(IsTestTest, Failure) {
   // This test belongs to the IsTestTest test case.
@@ -14,4 +30,6 @@ TEST(IsTestTest, Failure) {
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
+
+
 }
