614e001502759878b15ab322e003db37a7e5ddd7
dschlaep <daniel.schlaepfer@alumni.ethz.ch>
2017-11-21 17:19:05 -0500

    Include source files in unit tests
    - bad practice, see issue #82 which remains unsolved
    - I want this branch to be merged into master so that I can use the
    last set of commits as basis for development in other branches. sorry.

--- test/test_rands.cc
+++ test/test_rands.cc
@@ -15,9 +15,13 @@
 #include <time.h>
 #include <unistd.h>
 #include "../generic.h"
+#include "../generic.c"
 #include "../myMemory.h"
+#include "../myMemory.c"
 #include "../filefuncs.h"
+#include "../filefuncs.c"
 #include "../rands.h"
+#include "../rands.c"
 
 
 namespace {
