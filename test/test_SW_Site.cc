212b4643027ac17afc97fc800bde0b631bb653f5
candrews@usgs.gov <candrews@usgs.gov>
2017-12-21 12:25:12 -0700

    Create test file
    created test_SW_Site.cc file

---
+++ test/test_SW_Site.cc
@@ -0,0 +1,53 @@
+#include "gtest/gtest.h"
+#include <assert.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <errno.h>
+#include <float.h>
+#include <math.h>
+#include <memory.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+
+#include <typeinfo>  // for 'typeid'
+
+#include "../generic.h"
+#include "../myMemory.h"
+#include "../filefuncs.h"
+#include "../rands.h"
+#include "../Times.h"
+#include "../SW_Defines.h"
+#include "../SW_Times.h"
+#include "../SW_Files.h"
+#include "../SW_Carbon.h"
+#include "../SW_Site.h"
+#include "../SW_VegProd.h"
+#include "../SW_VegEstab.h"
+#include "../SW_Model.h"
+#include "../SW_SoilWater.h"
+#include "../SW_Weather.h"
+#include "../SW_Markov.h"
+#include "../SW_Sky.h"
+
+#include "sw_testhelpers.h"
+
+
+extern SW_CARBON SW_Carbon;
+extern SW_MODEL SW_Model;
+extern SW_VEGPROD SW_VegProd;
+
+
+
+namespace {
+
+    // Reset to previous global states
+    Reset_SOILWAT2_after_UnitTest();
+  }
+
+} // namespace
