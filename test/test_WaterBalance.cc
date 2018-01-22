dc0d3e19656ad2956ec8875d4cd9a8d5b092e496
dschlaep <daniel.schlaepfer@alumni.ethz.ch>
2018-01-22 11:38:37 -0500

    Scaffold for water balance unit tests
    - unit test file with description for water balance checks
    - addressing #133

---
+++ test/test_WaterBalance.cc
@@ -0,0 +1,68 @@
+#include "gtest/gtest.h"
+#include <assert.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <errno.h>
+#include <float.h>
+#include <math.h>
+#include <memory.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "../generic.h"
+#include "../myMemory.h"
+#include "../filefuncs.h"
+#include "../rands.h"
+#include "../Times.h"
+#include "../SW_Defines.h"
+#include "../SW_Times.h"
+#include "../SW_Files.h"
+#include "../SW_Carbon.h"
+#include "../SW_Site.h"
+#include "../SW_VegProd.h"
+#include "../SW_VegEstab.h"
+#include "../SW_Model.h"
+#include "../SW_SoilWater.h"
+#include "../SW_Weather.h"
+#include "../SW_Markov.h"
+#include "../SW_Sky.h"
+#include "../SW_Control.h"
+
+#include "sw_testhelpers.h"
+
+
+extern SW_CARBON SW_Carbon;
+extern SW_MODEL SW_Model;
+extern SW_VEGPROD SW_VegProd;
+
+
+
+namespace {
+  SW_CARBON *c = &SW_Carbon;
+  SW_VEGPROD *v  = &SW_VegProd;
+
+  /* Test daily water balance and water cycling:
+       i) Call function 'SW_CTL_main' which calls 'SW_CTL_run_current_year' for each year
+          which calls 'SW_SWC_water_flow' for each day
+      ii) Summarize checks added to debugging code of 'SW_SWC_water_flow' (which is
+          compiled if flag 'DEBUG' is defined)
+  */
+  TEST(ConsistencyTests, WaterBalance) {
+
+    // Run the simulation
+    SW_CTL_main();
+
+    // Collect and output from daily checks
+    EXPECT_EQ(0, 0);
+
+    // Reset to previous global state
+    Reset_SOILWAT2_after_UnitTest();
+  }
+
+} // namespace
