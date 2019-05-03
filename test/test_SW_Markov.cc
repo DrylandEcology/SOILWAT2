DrylandEcology/SOILWAT2

commit 5173b78b5f1c35cc6cb46948f108300d73cc1a3a

Adding Trace Changes: Change Markov files for Stepwat runs

SW_Markov.c
@@ -51,6 +51,57 @@ static RealD _vcov[2][2], _ucov[2];
 /*             Private Function Definitions            */
 /* --------------------------------------------------- */
 
+static void temp_correct(TimeInt doy,RealD *tmax, RealD *tmin, RealD *rain) {
+    
+    RealD tx, tn,cfxw,cfxd,cfnw,cfnd,rn;
+    
+    rn = *rain;
+    cfxw = SW_Markov.cfxw[doy];
+    cfnw = SW_Markov.cfnw[doy];
+    cfxd = SW_Markov.cfxd[doy];
+    cfnd = SW_Markov.cfnd[doy];
+    tx = *tmax;
+    tn = *tmin; 
+    
+    if (rn > 0.) {
+            if (tx < 0. ) { //if temp is less than 0 special case
+                tx = tx*((1.0 - cfxw)+1.0);
+            }
+            else { tx = tx*cfxw;
+            }
+            
+            
+            if (tn < 0) {//if temp is less than 0 special case
+                tn = tn*((1.0 - cfnw)+1.0);
+            }
+            else {
+            tn = tn*cfnw;
+            }
+    }
+        
+ //apply correction factor to temperature
+        if (rn <= 0.) {
+            if (tx < 0. ) {//if temp is less than 0 special case
+                tx = tx*((1.0 - cfxd)+1.0);
+            }
+            else {
+                tx = tx*cfxd;
+            }
+            
+            if (tn < 0.) { //if temp is less than 0 special case
+                tn = tn*((1.0 - cfnd)+1.0);
+            }
+           else {
+            tn = tn*cfnd;
+           }
+        }
+        
+        
+        *tmin = tn;
+        *tmax = tx;
+        
+}
+
 static void mvnorm(RealD *tmax, RealD *tmin) {
 	/* --------------------------------------------------- */
 	/* This proc is distilled from a much more general function
@@ -69,7 +120,7 @@ static void mvnorm(RealD *tmax, RealD *tmin) {
 	 *       C converts the floats transparently.
 	 */
 	RealD s, z1, z2, vc00 = _vcov[0][0], vc10 = _vcov[1][0], vc11 = _vcov[1][1];
-
+        
 	vc00 = sqrt(vc00);
 	vc10 = (GT(vc00, 0.)) ? vc10 / vc00 : 0;
 	s = vc10 * vc10;
@@ -77,8 +128,8 @@ static void mvnorm(RealD *tmax, RealD *tmin) {
 		LogError(logfp, LOGFATAL, "\nBad covariance matrix in mvnorm()");
 	vc11 = (EQ(vc11, s)) ? 0. : sqrt(vc11 -s);
 
-	z1 = RandNorm(0., 1.);
-	z2 = RandNorm(0., 1.);
+	z1 = RandNorm(0., 3.5);  
+	z2 = RandNorm(0., 3.5);   
 	*tmin = (vc10 * z1) + (vc11 * z2) + _ucov[1];
 	*tmax = vc00 * z1 + _ucov[0];
 
@@ -98,6 +149,10 @@ void SW_MKV_construct(void) {
 	m->dryprob = (RealD *) Mem_Calloc(MAX_DAYS, s, "SW_MKV_construct");
 	m->avg_ppt = (RealD *) Mem_Calloc(MAX_DAYS, s, "SW_MKV_construct");
 	m->std_ppt = (RealD *) Mem_Calloc(MAX_DAYS, s, "SW_MKV_construct");
+    m->cfxw = (RealD *) Mem_Calloc(MAX_DAYS, s, "SW_MKV_construct");
+	m->cfxd = (RealD *) Mem_Calloc(MAX_DAYS, s, "SW_MKV_construct");
+	m->cfnw = (RealD *) Mem_Calloc(MAX_DAYS, s, "SW_MKV_construct");
+	m->cfnd = (RealD *) Mem_Calloc(MAX_DAYS, s, "SW_MKV_construct");
 }
 
 void SW_MKV_today(TimeInt doy, RealD *tmax, RealD *tmin, RealD *rain) {
@@ -128,7 +183,7 @@ void SW_MKV_today(TimeInt doy, RealD *tmax, RealD *tmin, RealD *rain) {
 	_ucov[0] = SW_Markov.u_cov[week][0];
 	_ucov[1] = SW_Markov.u_cov[week][1];
 	mvnorm(tmax, tmin);
-
+    temp_correct(doy,tmax,tmin,rain);        
 }
 
 Bool SW_MKV_read_prob(void) {
@@ -137,7 +192,7 @@ Bool SW_MKV_read_prob(void) {
 	const int nitems = 5;
 	FILE *f;
 	int lineno = 0, day, x;
-	RealF wet, dry, avg, std;
+	RealF wet, dry, avg, std, cfxw, cfxd, cfnw, cfnd;
 
 	/* note that Files.read() must be called prior to this. */
 	MyFileName = SW_F_name(eMarkovProb);
@@ -149,7 +204,7 @@ Bool SW_MKV_read_prob(void) {
 		if (++lineno == MAX_DAYS)
 			break; /* skip extra lines */
 
-		x = sscanf(inbuf, "%d %f %f %f %f", &day, &wet, &dry, &avg, &std);
+		x = sscanf(inbuf, "%d %f %f %f %f %f %f %f %f", &day, &wet, &dry, &avg, &std, &cfxw, &cfxd, &cfnw, &cfnd);               
 		if (x < nitems) {
 			CloseFile(&f);
 			LogError(logfp, LOGFATAL, "\nToo few values in line %d file %s\n", lineno, MyFileName);
@@ -159,6 +214,10 @@ Bool SW_MKV_read_prob(void) {
 		v->dryprob[day] = dry;
 		v->avg_ppt[day] = avg;
 		v->std_ppt[day] = std;
+        v->cfxw[day] = cfxw;
+		v->cfxd[day] = cfxd;
+		v->cfnw[day] = cfnw;
+		v->cfnd[day] = cfnd;
 
 	}
 	CloseFile(&f);
@@ -338,6 +397,10 @@ void SW_MKV_SetMemoryRefs( void) {
 	NoteMemoryRef(SW_Markov.dryprob);
 	NoteMemoryRef(SW_Markov.avg_ppt);
 	NoteMemoryRef(SW_Markov.std_ppt);
+	NoteMemoryRef(SW_Markov.cfxw);
+	NoteMemoryRef(SW_Markov.cfxd);
+	NoteMemoryRef(SW_Markov.cfnw);
+	NoteMemoryRef(SW_Markov.cfnd);
 
 }
 

SW_Markov.h
@@ -29,6 +29,10 @@ typedef struct {
 	*dryprob, /* probability of being dry today */
 	*avg_ppt, /* mean precip (cm) */
 	*std_ppt, /* std dev. for precip */
+    *cfxw, /*correction factor for tmax for wet days */
+    *cfxd, /*correction factor for tmax for dry days */
+    *cfnw, /*correction factor for tmin for wet days */
+    *cfnd, /*correction factor for tmin for dry days */
 	u_cov[MAX_WEEKS][2], /* mean temp (max, min) Celsius */
 	v_cov[MAX_WEEKS][2][2]; /* covariance matrix */
 	int ppt_events; /* number of ppt events generated this year */

commit 551f3fb40e0435ef3eead27effd89fbb343a572b

Fix weather generator function `mvnorm`
- fully document function and code
- added unit tests

- simplified use of function, i.e., removed modul-level variables `_ucov` and `_vcov` and replaced with function arguments
-> easier unit testing

- fixed lack of constraint that tmin <= tmax

- fix bug introduced by commit 5173b78b5f1c35cc6cb46948f108300d73cc1a3a on Dec 14, 2015:
- this commit drew random variables from a normal distribution with standard deviation 3.5 instead of the required 1 -> the algorithm didn't work as expected and could produce values Tmin > Tmax
-> drawing a multivariate normal variate is based on multiple standard normal variates (now documented by references to published algorithms, e.g., Gentle 2009 Computational statistics. Springer

SW_Markov.c
@@ -46,7 +46,6 @@ SW_MARKOV SW_Markov; /* declared here, externed elsewhere */
 /* --------------------------------------------------- */
 
 static char *MyFileName;
-static RealD _vcov[2][2], _ucov[2];
 
 /* =================================================== */
 /* =================================================== */
@@ -104,7 +103,26 @@ static void temp_correct(TimeInt doy,RealD *tmax, RealD *tmin, RealD *rain) {
 
 }
 
-static void mvnorm(RealD *tmax, RealD *tmin) {
+/** Calculate multivariate normal variates for a set of
+    minimum and maximum temperature means, variances, and their covariance
+    for a specific day
+
+    @param wTmax Mean weekly maximum daily temperature (degree Celsius);
+           previously `_ucov[0]`
+    @param wTmin Mean weekly minimum daily temperature (degree Celsius);
+           previously `_ucov[1]`
+    @param wTmax_var Mean weekly variance of maximum daily temperature;
+           previously `vc00 = _vcov[0][0]`
+    @param wTmin_var Mean weekly variance of minimum daily temperature;
+           previously `vc11 = _vcov[1][1]`
+    @param wT_covar Mean weekly covariance between maximum and minimum
+           daily temperature; previously `vc10 = _vcov[1][0]`
+
+    @return Daily minimum (*tmin) and maximum (*tmax) temperature.
+*/
+
+static void mvnorm(RealD *tmax, RealD *tmin, RealD wTmax, RealD wTmin,
+	RealD wTmax_var, RealD wTmin_var, RealD wT_covar) {
 	/* --------------------------------------------------- */
 	/* This proc is distilled from a much more general function
 	 * in the original fortran version which was prepared to
@@ -121,20 +139,45 @@ static void mvnorm(RealD *tmax, RealD *tmin) {
 	 * cwb - 24-Oct-03 -- Note the switch to double (RealD).
 	 *       C converts the floats transparently.
 	 */
-	RealD s, z1, z2, vc00 = _vcov[0][0], vc10 = _vcov[1][0], vc11 = _vcov[1][1];
+	RealD s, z1, z2, wTmax_sd, vc10, vc11;
 
-	vc00 = sqrt(vc00);
-	vc10 = (GT(vc00, 0.)) ? vc10 / vc00 : 0;
+	// Gentle, J. E. 2009. Computational statistics. Springer, Dordrecht; New York.
+	// ==> mvnorm = mean + A * z
+	// where
+	//   z = vector of standard normal variates
+	//   A = Cholesky factor or the square root of the variance-covariance matrix
+
+	// 2-dimensional case:
+	//   mvnorm1 = mean1 + sd1 * z1
+	//   mvnorm2 = mean2 + sd2 * (rho * z1 + sqrt(1 - rho^2) * z2)
+	//      where rho(Pearson) = covariance / (sd1 * sd2)
+
+	// mvnorm2 = sd2 * rho * z1 + sd2 * sqrt(1 - rho^2) * z2
+	// mvnorm2 = covar / sd1 * z1 + sd2 * sqrt(1 - covar ^ 2 / (var1 * var2)) * z2
+	// mvnorm2 = covar / sd1 * z1 + sqrt(var2 - covar ^ 2 / var1) * z2
+	// mvnorm2 = vc10 * z1 + vc11 * z2
+	// with
+	//   vc10 = covar / sd1
+	//   s = covar ^ 2 / var1
+	//   vc11 = sqrt(var2 - covar ^ 2 / var1)
+
+	// Generate two independent standard normal random numbers
+	z1 = RandNorm(0., 1., &markov_rng);
+	z2 = RandNorm(0., 1., &markov_rng);
+
+	wTmax_sd = sqrt(wTmax_var);
+	vc10 = (GT(wTmax_sd, 0.)) ? wT_covar / wTmax_sd : 0;
 	s = vc10 * vc10;
-	if (GT(s,vc11))
+
+	if (GT(s, wTmin_var)) {
 		LogError(logfp, LOGFATAL, "\nBad covariance matrix in mvnorm()");
-	vc11 = (EQ(vc11, s)) ? 0. : sqrt(vc11 -s);
+	}
 
-	z1 = RandNorm(0., 3.5, &markov_rng);
-	z2 = RandNorm(0., 3.5, &markov_rng);
-	*tmin = (vc10 * z1) + (vc11 * z2) + _ucov[1];
-	*tmax = vc00 * z1 + _ucov[0];
+	vc11 = (EQ(wTmin_var, s)) ? 0. : sqrt(wTmin_var - s);
 
+	// mvnorm = mean + A * z
+	*tmax = wTmax_sd * z1 + wTmax;
+	*tmin = (vc10 * z1) + (vc11 * z2) + wTmin;
 }
 
 /* =================================================== */
@@ -229,11 +272,15 @@ void SW_MKV_today(TimeInt doy, RealD *tmax, RealD *tmin, RealD *rain) {
 
 	/* Calculate temperature */
 	week = Doy2Week(doy+1);
-	memcpy(_vcov, &SW_Markov.v_cov[week], 4 * sizeof(RealD));
-	_ucov[0] = SW_Markov.u_cov[week][0];
-	_ucov[1] = SW_Markov.u_cov[week][1];
-	mvnorm(tmax, tmin);
 	temp_correct(doy,tmax,tmin,rain);
+	mvnorm(tmax, tmin,
+		SW_Markov.u_cov[week][0],    // mean weekly maximum daily temp
+		SW_Markov.u_cov[week][1],    // mean weekly minimum daily temp
+		SW_Markov.v_cov[week][0][0], // mean weekly variance of maximum daily temp
+		SW_Markov.v_cov[week][1][1], // mean weekly variance of minimum daily temp
+		SW_Markov.v_cov[week][1][0]  // mean weekly covariance of min/max daily temp
+	);
+
 }
 
 Bool SW_MKV_read_prob(void) {

test/test_SW_Markov.cc
@@ -1,0 +1,109 @@
+#include "gtest/gtest.h"
+#include <assert.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <errno.h>
+#include <float.h>
+#include <math.h>
+#include <memory.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "../generic.h"
+#include "../myMemory.h"
+#include "../filefuncs.h"
+#include "../rands.h"
+#include "../Times.h"
+#include "../SW_Defines.h"
+#include "../SW_Times.h"
+#include "../SW_Files.h"
+#include "../SW_Carbon.h"
+#include "../SW_Site.h"
+#include "../SW_VegProd.h"
+#include "../SW_VegEstab.h"
+#include "../SW_Model.h"
+#include "../SW_SoilWater.h"
+#include "../SW_Weather.h"
+#include "../SW_Markov.h"
+#include "../SW_Sky.h"
+
+#include "sw_testhelpers.h"
+
+
+extern SW_MODEL SW_Model;
+extern SW_MARKOV SW_Markov;
+
+extern void (*test_mvnorm)(RealD *, RealD *, RealD, RealD, RealD, RealD, RealD);
+
+
+namespace {
+  SW_MARKOV *m = &SW_Markov;
+
+  // Test the SW_MARKOV constructor 'SW_MKV_construct'
+  TEST(WGTest, Constructor) {
+    SW_MKV_construct();
+
+    // Check that at least first array elements are initialized to zero
+    EXPECT_DOUBLE_EQ(0., m->wetprob[0]);
+    EXPECT_DOUBLE_EQ(0., m->dryprob[0]);
+    EXPECT_DOUBLE_EQ(0., m->avg_ppt[0]);
+    EXPECT_DOUBLE_EQ(0., m->std_ppt[0]);
+    EXPECT_DOUBLE_EQ(0., m->cfxw[0]);
+    EXPECT_DOUBLE_EQ(0., m->cfxd[0]);
+    EXPECT_DOUBLE_EQ(0., m->cfnw[0]);
+    EXPECT_DOUBLE_EQ(0., m->cfnd[0]);
+
+    // Reset to previous global state
+    // Reset_SOILWAT2_after_UnitTest();
+  }
+
+
+  // Test drawing multivariate normal variates for daily maximum/minimum temp
+  TEST(WGTest, mvnorm) {
+    short k, n = 3;
+    RealD tmax = 0., tmin = 0.,
+      val[n] = {-10., 0., 10.};
+
+
+    for (k = 0; k < n; k++) {
+      // Case: wtmax = wtmin, variance = 0, covar = 0 ==> input = output
+      (test_mvnorm)(&tmax, &tmin, val[k], val[k], 0., 0., 0.);
+      EXPECT_DOUBLE_EQ(tmax, val[k]);
+      EXPECT_DOUBLE_EQ(tmin, val[k]);
+      EXPECT_DOUBLE_EQ(tmin, tmax);
+
+      // Case: wtmax = wtmin, variance = 0, covar > 0 ==> input = output
+      (test_mvnorm)(&tmax, &tmin, val[k], val[k], 0., 0., 1.);
+      EXPECT_DOUBLE_EQ(tmax, val[k]);
+      EXPECT_DOUBLE_EQ(tmin, val[k]);
+      EXPECT_DOUBLE_EQ(tmin, tmax);
+
+      // Case: wtmax > wtmin, variance > 0, covar > 0 ==> tmin <= tmax
+      (test_mvnorm)(&tmax, &tmin, val[k] + 1., val[k], 1., 1., 1.);
+      EXPECT_LE(tmin, tmax);
+
+      // Case: wtmax < wtmin, variance > 0, covar > 0 ==> tmin == tmax
+      (test_mvnorm)(&tmax, &tmin, val[k] - 1., val[k], 1., 1., 1.);
+      EXPECT_DOUBLE_EQ(tmin, tmax);
+    }
+
+    // Reset to previous global state
+    Reset_SOILWAT2_after_UnitTest();
+  }
+
+  TEST(WGTest, mvnormDeathTest) {
+    RealD tmax = 0., tmin = 0.;
+
+    // Case: (wT_covar ^ 2 / wTmax_var) > wTmin_var --> LOGFATAL
+    EXPECT_DEATH_IF_SUPPORTED(
+      (test_mvnorm)(&tmax, &tmin, 0., 0., 1., 1., 2.),
+      "@ generic.c LogError");
+  }
+
+} // namespace