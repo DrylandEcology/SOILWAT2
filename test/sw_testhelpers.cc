95c5fca5ba04f8ea56f0c72ae3dfb7422236a502
Daniel Schlaepfer <daniel.schlaepfer@alumni.ethz.ch>
2020-04-17 16:54:44 -0400

    Update `Reset_SOILWAT2_after_UnitTest` to set simulation time
    - addressing #272
    
    - simulation time is normally set with `SW_CTL_main()`, but many unit tests don't call `SW_CTL_main()`

--- test/sw_testhelpers.cc
+++ test/sw_testhelpers.cc
@@ -49,9 +49,18 @@ void Reset_SOILWAT2_after_UnitTest(void) {
   SW_CTL_init_model(_firstfile);
   SW_CTL_obtain_inputs();
 
-  // Next two function calls will require SW_Output.c (see issue #85 'Make SW_Output.c comptabile with c++ to include in unit testing code')
+
+  // Next two function calls will require SW_Output.c
+  //   (see issue #85 'Make SW_Output.c comptabile with c++ to include in unit testing code')
   // SW_OUT_set_ncol();
   // SW_OUT_set_colnames();
+
+
+  // Set year to first (Gregorian) calendar year of testing simulation:
+  // In a regular SOILWAT2 run, this is set via:
+  //   SW_CTL_main > SW_CTL_run_current_year >
+  //     _begin_year > SW_MDL_new_year > Time_new_year
+  Time_new_year(SW_Model.startyr);
 }
 
 
