59864a8dc6d13592a07e0d182c408f3e3b7a0cf0
Daniel Schlaepfer <daniel.schlaepfer@alumni.ethz.ch>
2021-09-07 09:10:08 -0400

    Update script to locally check SOILWAT2 across compilers
    - gcc10 and gcc11 are getting stuck at `SWFlowTempDeathTest.SoilTemperatureInitDeathTest` when running with leak sanitizer
    - improved progress messages

--- tools/check_SOILWAT2.sh
+++ tools/check_SOILWAT2.sh
@@ -4,8 +4,14 @@
 # - is currently set up for use with macports
 # - expects clang as default compiler
 
+# Problems:
+# - gcc10 and gcc11 are getting stuck at
+#   `SWFlowTempDeathTest.SoilTemperatureInitDeathTest` when running with
+#   sanitizer "ASAN_OPTIONS=detect_leaks=1"
+
 
 #--- List of (builtin and macport) compilers
+
 declare -a port_compilers=(
   "none"
   "mp-gcc7" "mp-gcc9" "mp-gcc10" "mp-gcc11"
@@ -22,12 +28,16 @@ declare -a cxxs=(
   "clang++" "clang++" "clang++" "clang++" "clang++"
 )
 
+
 ncomp=${#ccs[@]}
 
 
 #--- Loop through tests and compilers
 for ((k = 0; k < ncomp; k++)); do
-  echo $'\n'$'\n'$'\n'Test SOILWAT2 with ${port_compilers[k]} ------------------
+  echo $'\n'$'\n'$'\n'\
+       ==================================================$'\n'\
+       Test SOILWAT2 with compiler \'${port_compilers[k]}\'$'\n'\
+       ==================================================
 
   echo $'\n'Set compiler ...
   if [ "${port_compilers[k]}" != "none" ]; then
@@ -36,32 +46,51 @@ for ((k = 0; k < ncomp; k++)); do
 
   echo $'\n'`"${ccs[k]}" --version`
   echo $'\n'`"${cxxs[k]}" --version`
-  echo $'\n'
+  echo $'\n'\
+       -----------------------------------------------------
 
   CC=${ccs[k]} CXX=${cxxs[k]} make compiler_version
 
-  echo $'\n'$'\n'Run binary with ${port_compilers[k]} ...
+
+  echo $'\n'$'\n'\
+       --------------------------------------------------$'\n'\
+       Run binary with ${port_compilers[k]} ...$'\n'\
+       --------------------------------------------------
   CC=${ccs[k]} CXX=${cxxs[k]} make clean bin bint_run
   CC=${ccs[k]} CXX=${cxxs[k]} make clean bin_debug_severe bint_run
   if command -v valgrind >/dev/null 2>&1; then
     CC=${ccs[k]} CXX=${cxxs[k]} make bind_valgrind bint_run
   fi
 
-  echo $'\n'$'\n'Run tests with ${port_compilers[k]} ...
+
+  echo $'\n'$'\n'\
+       --------------------------------------------------$'\n'\
+       Run tests with ${port_compilers[k]} ...$'\n'\
+       --------------------------------------------------
   CC=${ccs[k]} CXX=${cxxs[k]} make clean test test_run
   CC=${ccs[k]} CXX=${cxxs[k]} make clean test_severe test_run
 
-  echo $'\n'$'\n'Run sanitizer on tests with ${port_compilers[k]}: exclude known leaks ...
+
+  echo $'\n'$'\n'\
+       --------------------------------------------------$'\n'\
+       Run sanitizer on tests with ${port_compilers[k]}: exclude known leaks ...$'\n'\
+       --------------------------------------------------
   CC=${ccs[k]} CXX=${cxxs[k]} ASAN_OPTIONS=detect_leaks=1 LSAN_OPTIONS=suppressions=.LSAN_suppr.txt make clean test_severe test_run
 
 
-  echo $'\n'$'\n'Unset compiler ...
+  echo $'\n'$'\n'\
+       --------------------------------------------------$'\n'\
+       Unset compiler ...$'\n'\
+       --------------------------------------------------
   if [ "${port_compilers[k]}" != "none" ]; then
     sudo port select --set ${ccs[k]} none
   fi
 done
 
-echo Complete!$'\n'
+echo $'\n'$'\n'\
+     --------------------------------------------------$'\n'\
+     Complete!$'\n'\
+     --------------------------------------------------$'\n'
 
 
 #--- Cleanup
