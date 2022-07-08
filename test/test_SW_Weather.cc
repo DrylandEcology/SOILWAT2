3466a4ae1539c03e33b95ed53b24551a81514720
Nicholas Persley <nspersley20@gmail.com>
2022-07-07 22:56:37 -0400

    Created three new unit tests for `readAllWeather()`
    - Created unit tests for the following conditions of `readAllWeather()`:
    	* Use of weather generator to fill in missing days
    	* Use of weather generator to fill in missing years
    	* Only use weather generator to fill weather data

--- test/test_SW_Weather.cc
+++ test/test_SW_Weather.cc
@@ -17,6 +17,8 @@
 
 #include "../SW_Weather.h"
 #include "sw_testhelpers.h"
+#include "../SW_Markov.h"
+#include "../SW_Model.h"
 
 namespace {
 
@@ -29,6 +31,74 @@ namespace {
         EXPECT_NEAR(SW_Weather.allHist[0]->temp_avg[0], -8.095000, tol6);
         EXPECT_NEAR(SW_Weather.allHist[0]->temp_min[0], -15.670000, tol6);
         EXPECT_NEAR(SW_Weather.allHist[0]->ppt[0], .220000, tol6);
+        
+    }
+
+    TEST(ReadAllWeatherTest, SomeMissingValuesDays) {
+        
+        SW_Weather.use_weathergenerator = swTRUE;
+        SW_MKV_setup();
+        strcpy(SW_Weather.name_prefix, "Input/data_weather_missing/weath");
+        
+        SW_WTH_read();
+        
+        EXPECT_TRUE(!missing(SW_Weather.allHist[0]->temp_max[0]));
+        EXPECT_TRUE(!missing(SW_Weather.allHist[0]->temp_max[1]));
+        EXPECT_TRUE(!missing(SW_Weather.allHist[0]->temp_max[2]));
+        EXPECT_TRUE(!missing(SW_Weather.allHist[0]->temp_max[3]));
+        EXPECT_TRUE(!missing(SW_Weather.allHist[0]->temp_max[4]));
+        EXPECT_TRUE(!missing(SW_Weather.allHist[0]->temp_max[365]));
+        
+        Reset_SOILWAT2_after_UnitTest();
+        
+    }
+
+    TEST(ReadAllWeatherTest, SomeMissingValuesYears) {
+        
+        int year, day;
+        
+        SW_Weather.use_weathergenerator = swTRUE;
+        strcpy(SW_Weather.name_prefix, "Input/data_weather_missing/weath");
+        SW_MKV_setup();
+        
+        readAllWeather(SW_Weather.allHist, 1981, 2);
+        
+        SW_Model.startyr = 1981;
+        SW_Weather.n_years = 2;
+        
+        for(year = 0; year < 2; year++) {
+            for(day = 0; day < 365; day++) {
+                EXPECT_TRUE(!missing(SW_Weather.allHist[year]->temp_max[day]));
+            }
+        }
+        
+        SW_Model.startyr = 1980;
+        SW_Weather.n_years = 31;
+        
+        Reset_SOILWAT2_after_UnitTest();
+        
+    }
+
+    TEST(ReadAllWeatherTest, WeatherGeneratorOnly) {
+        
+        int year, day;
+        
+        SW_Weather.use_weathergenerator = swTRUE;
+        SW_Weather.use_weathergenerator_only = swTRUE;
+        
+        SW_MKV_setup();
+        
+        strcpy(SW_Weather.name_prefix, "Input/data_weather_nonexisting/weath");
+        
+        SW_WTH_read();
+        
+        for(year = 0; year < 31; year++) {
+            for(day = 0; day < 365; day++) {
+                EXPECT_TRUE(!missing(SW_Weather.allHist[year]->temp_max[day]));
+            }
+        }
+        
+        Reset_SOILWAT2_after_UnitTest();
         
     }
 
