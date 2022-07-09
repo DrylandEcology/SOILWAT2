ab4fb949a6ba022f7f7308ba5db6715b9eeaf9b9
Nicholas Persley <nspersley20@gmail.com>
2022-07-08 23:00:50 -0400

    Added comments for existing weather tests

--- test/test_SW_Weather.cc
+++ test/test_SW_Weather.cc
@@ -27,6 +27,8 @@ namespace {
         // Testing to fill allHist from `SW_Weather`
         readAllWeather(SW_Weather.allHist, 1980, SW_Weather.n_years);
         
+        // Test first day of first year in `allHist` to make sure correct
+        // temperature max/min/avg and precipitation values
         EXPECT_NEAR(SW_Weather.allHist[0]->temp_max[0], -0.520000, tol6);
         EXPECT_NEAR(SW_Weather.allHist[0]->temp_avg[0], -8.095000, tol6);
         EXPECT_NEAR(SW_Weather.allHist[0]->temp_min[0], -15.670000, tol6);
@@ -38,10 +40,14 @@ namespace {
         
         SW_Weather.use_weathergenerator = swTRUE;
         SW_MKV_setup();
+        
+        // Change directory to get input files with some missing data
         strcpy(SW_Weather.name_prefix, "Input/data_weather_missing/weath");
         
         SW_WTH_read();
         
+        // With the use of 1980's missing values, test a few days of the year
+        // to make sure they are filled using the weather generator
         EXPECT_TRUE(!missing(SW_Weather.allHist[0]->temp_max[0]));
         EXPECT_TRUE(!missing(SW_Weather.allHist[0]->temp_max[1]));
         EXPECT_TRUE(!missing(SW_Weather.allHist[0]->temp_max[2]));
@@ -66,6 +72,7 @@ namespace {
         SW_Model.startyr = 1981;
         SW_Weather.n_years = 2;
         
+        // Check everyday's value and test if it's `MISSING`
         for(year = 0; year < 2; year++) {
             for(day = 0; day < 365; day++) {
                 EXPECT_TRUE(!missing(SW_Weather.allHist[year]->temp_max[day]));
@@ -88,10 +95,12 @@ namespace {
         
         SW_MKV_setup();
         
+        // Change directory to get input files with some missing data
         strcpy(SW_Weather.name_prefix, "Input/data_weather_nonexisting/weath");
         
         SW_WTH_read();
         
+        // Check everyday's value and test if it's `MISSING`
         for(year = 0; year < 31; year++) {
             for(day = 0; day < 365; day++) {
                 EXPECT_TRUE(!missing(SW_Weather.allHist[year]->temp_max[day]));
