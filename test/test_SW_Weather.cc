b1cbd6aaec19033675aa8f5b09e60d59ad0a21fd
Nicholas Persley <nspersley20@gmail.com>
2022-07-09 22:39:45 -0400

    Added unit tests for climate functions in `test_SW_Weather.cc`
    - Added unit test to test functions:
    	* `findDriestQtr()`, `calcSiteClimate()` and `averageAcrossYears()`

--- test/test_SW_Weather.cc
+++ test/test_SW_Weather.cc
@@ -137,6 +137,177 @@ namespace {
         Reset_SOILWAT2_after_UnitTest();
 
     }
+    TEST(AverageAcrossYearsTest, FileValues) {
+        
+        // This test relies on allHist from `SW_WEATHER` being already filled
+        
+        double meanMonthlyAvgAnn[MAX_MONTHS];
+        double meanMonthlyMaxAnn[MAX_MONTHS];
+        double meanMonthlyMinAnn[MAX_MONTHS];
+        double meanMonthlyPPTAnn[MAX_MONTHS];
+        double JulyMinTemp[31];
+        int frostFreeDays[31];
+        double degreeAbove65[31];
+        double PPTJuly[31];
+        double meanTempDryQuarter[31];
+        double minTempFebruary[31];
+        double sdCheatgrass[3];
+        double sdC4[3];
+        double MMP_cm[31];
+        double MMT_C[31];
+        
+        double MAP_cm;
+        double MAT_C;
+        
+        double AnnTemp_C[31];
+        double AnnPPT_cm[31];
+        
+        double **arrayPPTYear;
+        arrayPPTYear = new double*[MAX_MONTHS];
+        
+        double **arrayTempAvgYear = new double*[MAX_MONTHS];
+        
+        double **arrayTempMinYear = new double*[MAX_MONTHS];
+        
+        double **arrayTempMaxYear = new double*[MAX_MONTHS];
+        
+        for(int month = 0; month < MAX_MONTHS; month++) {
+            arrayPPTYear[month] = new double[31];
+            arrayTempAvgYear[month] = new double[31];
+            arrayTempMinYear[month] = new double[31];
+            arrayTempMaxYear[month] = new double[31];
+            for(int year = 0; year < 31; year++) {
+                
+                arrayPPTYear[month][year] = 0.;
+                arrayTempAvgYear[month][year] = 0.;
+                arrayTempMinYear[month][year] = 0.;
+                arrayTempMaxYear[month][year] = 0.;
+            }
+        }
+        // 1980 is start year of the simulation
+        calcSiteClimate(SW_Weather.allHist, arrayTempAvgYear, arrayTempMaxYear, arrayTempMinYear,
+                        arrayPPTYear, MMP_cm, MMT_C, JulyMinTemp, frostFreeDays, degreeAbove65, sdC4,
+                        PPTJuly, meanTempDryQuarter, minTempFebruary, sdCheatgrass, 31, 1980);
+        
+        averageAcrossYears(arrayTempAvgYear, arrayTempMaxYear, arrayTempMinYear, arrayPPTYear,
+                        meanMonthlyAvgAnn, meanMonthlyMaxAnn, meanMonthlyMinAnn, meanMonthlyPPTAnn,
+                        &MAP_cm, &MAT_C, AnnTemp_C, AnnPPT_cm, 31);
+        
+        EXPECT_NEAR(meanMonthlyAvgAnn[0], -9.325551, tol6);
+        EXPECT_NEAR(meanMonthlyMaxAnn[0], -2.714381, tol6);
+        EXPECT_NEAR(meanMonthlyMinAnn[0], -15.936722, tol6);
+        EXPECT_NEAR(meanMonthlyPPTAnn[0], 0.221530, tol6);
+        
+        for(int month = 0; month < MAX_MONTHS; month++) {
+            delete[] arrayPPTYear[month];
+            delete[] arrayTempAvgYear[month];
+            delete[] arrayTempMinYear[month];
+            delete[] arrayTempMaxYear[month];
+        }
+        
+        delete[] arrayPPTYear;
+        delete[] arrayTempAvgYear;
+        delete[] arrayTempMinYear;
+        delete[] arrayTempMaxYear;
+        
+    }
+
+    TEST(calcSiteClimateTest, FileValues) {
+        
+        // This test relies on allHist from `SW_WEATHER` being already filled
+        
+        double JulyMinTemp[31];
+        int frostFreeDays[31];
+        double degreeAbove65[31];
+        double PPTJuly[31];
+        double meanTempDryQuarter[31];
+        double minTempFebruary[31];
+        double sdCheatgrass[3];
+        double sdC4[3];
+        double MMP_cm[31];
+        double MMT_C[31];
+        
+        double **arrayPPTYear;
+        arrayPPTYear = new double*[MAX_MONTHS];
+        
+        double **arrayTempAvgYear;
+        arrayTempAvgYear = new double*[MAX_MONTHS];
+        
+        double **arrayTempMinYear;
+        arrayTempMinYear = new double*[MAX_MONTHS];
+        
+        double **arrayTempMaxYear;
+        arrayTempMaxYear = new double*[MAX_MONTHS];
+        
+        for(int month = 0; month < MAX_MONTHS; month++) {
+            arrayPPTYear[month] = new double[31];
+            arrayTempAvgYear[month] = new double[31];
+            arrayTempMinYear[month] = new double[31];
+            arrayTempMaxYear[month] = new double[31];
+            
+            for(int year = 0; year < 31; year++) {
+                
+                arrayPPTYear[month][year] = 0.;
+                arrayTempAvgYear[month][year] = 0.;
+                arrayTempMinYear[month][year] = 0.;
+                arrayTempMaxYear[month][year] = 0.;
+            }
+        }
+        // 1980 is start year of the simulation
+        calcSiteClimate(SW_Weather.allHist, arrayTempAvgYear, arrayTempMaxYear, arrayTempMinYear,
+                        arrayPPTYear, MMP_cm, MMT_C, JulyMinTemp, frostFreeDays, degreeAbove65, sdC4,
+                        PPTJuly, meanTempDryQuarter, minTempFebruary, sdCheatgrass, 31, 1980);
+        
+        EXPECT_NEAR(arrayTempAvgYear[0][0], -8.432581, tol6);
+        
+        for(int month = 0; month < MAX_MONTHS; month++) {
+            delete[] arrayPPTYear[month];
+            delete[] arrayTempAvgYear[month];
+            delete[] arrayTempMinYear[month];
+            delete[] arrayTempMaxYear[month];
+        }
+        
+        delete[] arrayPPTYear;
+        delete[] arrayTempAvgYear;
+        delete[] arrayTempMinYear;
+        delete[] arrayTempMaxYear;
+        
+    }
+
+    TEST(DriestQuarterTest, OneYear) {
+        
+        double monthlyPPT[MAX_MONTHS] = {.5, .5, .1, .4, .9, 1.0, 1.2, 6.5, 7.5, 1.2, 4., .6};
+        double monthlyTemp[MAX_MONTHS] = {-3.2, -.4, 1.2, 3.5, 7.5, 4.5, 6.5, 8.2, 2.0, 3., .1, -.3};
+        double result[31]; // 31 = number of years in simulation
+        
+        double **arrayPPTYear;
+        arrayPPTYear = new double*[MAX_MONTHS];
+        
+        double **arrayTempAvgYear = new double*[MAX_MONTHS];
+        
+        for(int month = 0; month < MAX_MONTHS; month++) {
+            arrayPPTYear[month] = new double[31];
+            arrayTempAvgYear[month] = new double[31];
+            for(int year = 0; year < 31; year++) {
+                
+                arrayPPTYear[month][year] = monthlyPPT[month];
+                arrayTempAvgYear[month][year] = monthlyTemp[month];
+            }
+        }
+        // 1980 is start year of the simulation
+        findDriestQtr(arrayTempAvgYear, arrayPPTYear, result, 31, 1980);
+        
+        EXPECT_DOUBLE_EQ(result[0], 1.4333333333333333);
+        
+        for(int month = 0; month < MAX_MONTHS; month++) {
+            delete[] arrayPPTYear[month];
+            delete[] arrayTempAvgYear[month];
+        }
+        
+        delete[] arrayPPTYear;
+        delete[] arrayTempAvgYear;
+        
+    }
 
     TEST(WeatherReadTest, Initialization) {
         
