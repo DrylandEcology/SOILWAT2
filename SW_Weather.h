a40886adfdadabe729cdcc2413462daee3f035d0
Nicholas Persley <nspersley20@gmail.com>
2022-07-22 12:21:59 -0400

    Added documentation for new weather related structs

--- SW_Weather.h
+++ SW_Weather.h
@@ -57,16 +57,50 @@ typedef struct {
 	snowRunoff, surfaceRunoff, surfaceRunon, soil_inf, et, aet, pet, surfaceAvg, surfaceMax, surfaceMin;
 } SW_WEATHER_OUTPUTS;
 
+/**
+ A structure holding all variables that are output to the function `calcSiteClimate()` and is input to the function `averageClimateAcrossYears()`
+ 
+ @note Arrays are to be of size `numYears` which is variable (31 years when testing)
+ @note Arrays hold values for all simulation years
+ */
 typedef struct {
-    RealD **meanMonthlyTemp_C, **maxMonthlyTemp_C, **minMonthlyTemp_C, **monthlyPPT_cm,
-    *annualPPT_cm, *meanAnnualTemp_C, *JulyMinTemp, *frostFreeDays_days, *ddAbove65F_degday,
-    *JulyPPT_mm, *meanTempDriestQuarter_C, *minTempFebruary_C;
+    RealD **meanMonthlyTemp_C,              /**< 2D array containing monthly mean average daily air temperature (deg;C)*/
+    **maxMonthlyTemp_C,                     /**< 2D array containing monthly mean max daily air temperature (deg;C)*/
+    **minMonthlyTemp_C,                     /**< 2D array containing monthly mean min daily air temperature (deg;C)*/
+    **monthlyPPT_cm,                        /**< 2D array containing monthly amount precipitation (cm)*/
+    *annualPPT_cm,                          /**< Array containing annual precipitation amount [cm]*/
+    *meanAnnualTemp_C,                      /**< Array containing annual mean temperatures [C]*/
+    *JulyMinTemp,                           /**< Array containing minimum July temperatures [C] */
+    *frostFreeDays_days,                    /**< Array containing the maximum consecutive days in a year without frost*/
+    *ddAbove65F_degday,                     /**< Array containing the amount of degree days [C x day] above 65 F */
+    *JulyPPT_mm,                            /**< Array containing July precipitation amount (mm) for all years */
+    *meanTempDriestQuarter_C,               /**< Array containing the average temperature of the driest quarter of the year*/
+    *minTempFebruary_C;                     /**< Array containing the mean minimum temperature in february*/
 } SW_CLIMATE_CALC;
 
+/**
+ A structure holding all variables that are output to the function `averageClimateAcrossYears()`
+ 
+ @note If a description mentions a size, it is not variable and has to be the specified size
+ @note Averages are taken across all simulation years
+ */
 typedef struct {
-    RealD *meanMonthlyTempAnn, *maxMonthlyTempAnn, *minMonthlyTempAnn, *meanMonthlyPPTAnn,
-    *sdC4, *sdCheatgrass, MAT_C, MAP_cm, JulyPPTAnn_mm, meanTempDriestQuarterAnn_C, minTempFebruaryAnn_C,
-    ddAbove65F_degdayAnn, frostFreeAnn, JulyMinTempAnn;
+    RealD *meanMonthlyTempAnn,              /**< Array of size MAX_MONTHS containing sum of monthly mean temperatures*/
+    *maxMonthlyTempAnn,                     /**< Array of size MAX_MONTHS containing sum of monthly maximum temperatures*/
+    *minMonthlyTempAnn,                     /**< Array of size MAX_MONTHS containing sum of monthly minimum temperatures*/
+    *meanMonthlyPPTAnn,                     /**< Array of size MAX_MONTHS containing sum of monthly mean precipitation*/
+    *sdC4,                                  /**< Array of size three holding the standard deviations of minimum July temperature (0),
+                                             frost free days (1), number of days above 65F (2)*/
+    *sdCheatgrass,                          /**< Array of size three holding the standard deviations of July precipitation (0), mean
+                                              temperature of dry quarter (1), mean minimum temperature of February (2)*/
+    MAT_C,                                  /**< Value containing the average of yearly temperatures*/
+    MAP_cm,                                 /**< Value containing the average of yearly precipitation*/
+    JulyPPTAnn_mm,                          /**< Value containing average of July precipitation (mm)*/
+    meanTempDriestQuarterAnn_C,             /**< Value containing average of mean temperatures in the driest quarters of years*/
+    minTempFebruaryAnn_C,                   /**< Value containing average of minimum temperatures in February*/
+    ddAbove65F_degdayAnn,                   /**< Value containing average of total degrees above 65F (18.33C) throughout the year*/
+    frostFreeAnn,                           /**< Value containing average of most consectutive days in a year without frost*/
+    JulyMinTempAnn;                         /**< Value containing the average of lowest temperature in July*/
 } SW_CLIMATE_AVERAGES;
 
 typedef struct {
