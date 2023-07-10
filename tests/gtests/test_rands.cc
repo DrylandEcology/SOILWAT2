9d9fb70e4b22a66eecee731db3b437f8a2db5e4d
Nicholas Persley <nspersley20@gmail.com>
2023-07-10 12:17:24 -0400

    Remove test fixture from rand test

--- tests/gtests/test_rands.cc
+++ tests/gtests/test_rands.cc
@@ -291,7 +291,7 @@ namespace {
     }
   }
 
-  TEST_F(AllTestDeathTest, DISABLED_RNGBetaErrorsDeathTest) {
+  TEST(RNGBetaDeathTest, Errors) {
     pcg32_random_t error_rng;
     RandSeed(0u, 0u, &error_rng);
 
