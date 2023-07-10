afe4d322151fc73b2ba9600e8c129fe87bc825e6
Nicholas Persley <nspersley20@gmail.com>
2023-07-10 12:42:14 -0400

    Enable death test SWFlowTempMainSoilTemperatureFunctionDeathTest

--- tests/gtests/test_SW_Flow_lib_temp.cc
+++ tests/gtests/test_SW_Flow_lib_temp.cc
@@ -715,7 +715,7 @@ namespace {
   }
 
   // Test that main soil temperature functions fails when it is supposed to
-  TEST_F(AllTestDeathTest, DISABLED_SWFlowTempMainSoilTemperatureFunctionDeathTest) {
+  TEST_F(AllTestDeathTest, SWFlowTempMainSoilTemperatureFunctionDeathTest) {
 
     unsigned int nlyrs = 1, nRgr = 65;
     double airTemp = 25.0, pet = 5.0, aet = 4.0, biomass = 100., surfaceTemp = 15.,
