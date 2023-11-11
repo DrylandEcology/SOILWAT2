b0683b6e4f439976fb622e405627da81cb74727d
Nicholas Persley <nsp73@nau.edu>
2023-11-11 16:08:05 -0700

    New files - `SW_netCDF.c/h`
    - These files will deal with functionality in regards to netCDF inputs/outputs

---
+++ include/SW_netCDF.h
@@ -0,0 +1,16 @@
+#ifndef SWNETCDF_H
+#define SWNETCDF_H
+
+#include "include/SW_datastructs.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // SWNETCDF_H
