968a6e2efc5e956526be458173cc2b53b8a65986
Nicholas Persley <nsp73@nau.edu>
2023-11-11 17:26:33 -0700

    Add stub functions for branch
    - Create multiple stubs for functions that will be used in the branch `feature_nc_domain`
    - Silence warnings for the time being until they are filled
    
    `SW_NC_read_domain()` - Reads domain information and makes sure its consistent with the domain input file (domain.in)
    `SW_NC_check()` - Checks that the information is consistent with domain (domain.in)
    `SW_NC_create_domain_template()` - Creates a template domain for users to fill (will exit the program early for the user to fill)
    `SW_NC_create_template()` - Copy domain netCDF that will later be modified to contain values for a specific output variable
    `SW_NC_read_domain()` - Reads the given inputs from input netCDF's for specific variables
    `SW_NC_check_input_files()` - Make sure all given input files have consistent spacial data with the domain (domain.in)

--- include/SW_netCDF.h
+++ include/SW_netCDF.h
@@ -7,7 +7,17 @@
 extern "C" {
 #endif
 
-
+/* =================================================== */
+/*             Global Function Declarations            */
+/* --------------------------------------------------- */
+void SW_NC_read_domain(SW_DOMAIN* SW_Domain, LOG_INFO* LogInfo);
+void SW_NC_check(SW_DOMAIN* SW_Domain, const char* fileName,
+                 LOG_INFO* LogInfo);
+void SW_NC_create_domain_template(SW_DOMAIN* SW_Domain, LOG_INFO* LogInfo);
+void SW_NC_create_template(SW_DOMAIN* SW_Domain, LOG_INFO* LogInfo);
+void SW_NC_read_inputs(SW_ALL* sw, SW_DOMAIN* SW_Domain, unsigned long ncSUID,
+                       LOG_INFO* LogInfo);
+void SW_NC_check_input_files(SW_DOMAIN* SW_Domain, LOG_INFO* LogInfo);
 
 #ifdef __cplusplus
 }
--- src/SW_netCDF.c
+++ src/SW_netCDF.c
@@ -1,1 +1,106 @@
 #include "include/SW_netCDF.h"
+
+/* =================================================== */
+/*             Global Function Definitions             */
+/* --------------------------------------------------- */
+
+/**
+ * @brief Read domain netCDF, obtain CRS-related information,
+ *  and check that content is consistent with "domain.in"
+ *
+ * @param[out] SW_Domain Struct of type SW_DOMAIN holding constant
+ *  temporal/spatial information for a set of simulation runs
+ * @param[in] domFileName Name of the domain netCDF file
+ * @param[in,out] LogInfo Holds information dealing with logfile output
+*/
+void SW_NC_read_domain(SW_DOMAIN* SW_Domain, const char* domFileName,
+                       LOG_INFO* LogInfo) {
+
+    (void) SW_Domain;
+    (void) domFileName;
+    (void) LogInfo;
+}
+
+/**
+ * @brief Check that content is consistent with domain
+ *
+ * @param[in] SW_Domain Struct of type SW_DOMAIN holding constant
+ *  temporal/spatial information for a set of simulation runs
+ * @param[in] fileName Name of netCDF file to test
+ * @param[in,out] LogInfo Holds information dealing with logfile output
+*/
+void SW_NC_check(SW_DOMAIN* SW_Domain, const char* fileName,
+                 LOG_INFO* LogInfo) {
+
+    (void) SW_Domain;
+    (void) fileName;
+    (void) LogInfo;
+}
+
+/**
+ * @brief Create “domain_template.nc” if “domain.nc” does not exists
+ *
+ * @param[in] SW_Domain Struct of type SW_DOMAIN holding constant
+ *  temporal/spatial information for a set of simulation runs
+ * @param[in] domFileName File name in which the new netCDF file will have
+ * @param[in,out] LogInfo Holds information dealing with logfile output
+*/
+void SW_NC_create_domain_template(SW_DOMAIN* SW_Domain, const char* domFileName,
+                                  LOG_INFO* LogInfo) {
+    (void) SW_Domain;
+    (void) domFileName;
+    (void) LogInfo;
+}
+
+/**
+ * @brief Copy domain netCDF to a new file and make it ready for a new variable
+ *
+ * @param[in] fileName Name of the netCDF file to create
+ * @param[in] timeSize Size of "time" dimension
+ * @param[in] vertSize Size of "vertical" dimension
+ * @param[in] varName Name of variable to write
+ * @param[in] varAttributes Attributes that the new variable will contain
+ * @param[in,out] LogInfo  Holds information dealing with logfile output
+*/
+void SW_NC_create_template(const char* fileName, unsigned long timeSize,
+                           unsigned long vertSize, const char* varName,
+                           char* varAttributes[], LOG_INFO* LogInfo) {
+    (void) fileName;
+    (void) timeSize;
+    (void) vertSize;
+    (void) varName;
+    (void) varAttributes;
+    (void) LogInfo;
+}
+
+/**
+ * @brief Read values from netCDF input files for available variables and copy to SW_All
+ *
+ * @param[in,out] sw Comprehensive struct of type SW_ALL containing
+ *  all information in the simulation
+ * @param[in] SW_Domain Struct of type SW_DOMAIN holding constant
+ *  temporal/spatial information for a set of simulation runs
+ * @param[in] ncSUID Current simulation unit identifier for which is used
+ *  to get data from netCDF
+ * @param[in,out] LogInfo Holds information dealing with logfile output
+*/
+void SW_NC_read_inputs(SW_ALL* sw, SW_DOMAIN* SW_Domain, unsigned long ncSUID,
+                       LOG_INFO* LogInfo) {
+
+    (void) sw;
+    (void) SW_Domain;
+    (void) ncSUID;
+    (void) LogInfo;
+}
+
+/**
+ * @brief Check that all available netCDF input files are consistent with domain
+ *
+ * @param[in] SW_Domain Struct of type SW_DOMAIN holding constant
+ *  temporal/spatial information for a set of simulation runs
+ * @param[in,out] LogInfo Holds information dealing with logfile output
+*/
+void SW_NC_check_input_files(SW_DOMAIN* SW_Domain, LOG_INFO* LogInfo) {
+    (void) SW_Domain;
+    (void) LogInfo;
+}
