2fa7c5a013879e8596da020804cbd1a306151baa
Daniel Schlaepfer <daniel.schlaepfer@alumni.ethz.ch>
2024-07-26 09:58:26 -0400

    tools/run_tidy.sh is now less verbose
    - silence progress report
    - just output warnings/errors

--- tools/run_tidy.sh
+++ tools/run_tidy.sh
@@ -3,22 +3,22 @@
 
 #-------------------------------------------------------------------------------
 # Check text-based SOILWAT2
-make CPPFLAGS='-DSWDEBUG' tidy-bin
+make CPPFLAGS='-DSWDEBUG' tidy-bin 2>/dev/null
 
 
 #-------------------------------------------------------------------------------
 # Check nc-based SOILWAT2
-make CPPFLAGS='-DSWNETCDF -DSWUDUNITS -DSWDEBUG' tidy-bin
+make CPPFLAGS='-DSWNETCDF -DSWUDUNITS -DSWDEBUG' tidy-bin 2>/dev/null
 
 
 #-------------------------------------------------------------------------------
 # Check SOILWAT2 library for rSOILWAT2
-make CPPFLAGS='-DRSOILWAT' CFLAGS='-Iexternal/Rmock' tidy-bin
+make CPPFLAGS='-DRSOILWAT' CFLAGS='-Iexternal/Rmock' tidy-bin 2>/dev/null
 
 
 #-------------------------------------------------------------------------------
 # Check SOILWAT2 library for STEPWAT2
-make CPPFLAGS='-DSTEPWAT' tidy-bin
+make CPPFLAGS='-DSTEPWAT' tidy-bin 2>/dev/null
 
 
 #-------------------------------------------------------------------------------
