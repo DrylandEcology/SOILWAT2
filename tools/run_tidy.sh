86de92c8dcffb79d3b4cbf500193fa6f0b167e6b
Daniel Schlaepfer <daniel.schlaepfer@alumni.ethz.ch>
2024-07-31 15:56:01 -0400

    Update "tools/run_tidy.sh" to exit with correct code
    - cleaner report on what is happening
    - output only if a check fails
    - exit 1 if error

--- tools/run_tidy.sh
+++ tools/run_tidy.sh
@@ -1,29 +1,71 @@
 #!/bin/bash
 
-
 #-------------------------------------------------------------------------------
-# Check text-based SOILWAT2
-make CPPFLAGS='-DSWDEBUG' tidy-bin 2>/dev/null
+process_clangtidy_results() {
+    local status="$1"
+    local res="$2"
 
-
-#-------------------------------------------------------------------------------
-# Check nc-based SOILWAT2
-make CPPFLAGS='-DSWNETCDF -DSWUDUNITS -DSWDEBUG' tidy-bin 2>/dev/null
-
-
-#-------------------------------------------------------------------------------
-# Check SOILWAT2 library for rSOILWAT2
-make CPPFLAGS='-DRSOILWAT' CFLAGS='-Iexternal/Rmock' tidy-bin 2>/dev/null
-
-
-#-------------------------------------------------------------------------------
-# Check SOILWAT2 library for STEPWAT2
-make CPPFLAGS='-DSTEPWAT' tidy-bin 2>/dev/null
+    if [ $status -eq 0 ]; then
+        # success: clang-tidy found no check failures
+        return 0
+    else
+        # failure: clang-tidy produced check output
+        echo "$res"
+        exit 1
+    fi
+}
 
 
 #-------------------------------------------------------------------------------
-# Check SOILWAT2 tests with all extra flags
-make CPPFLAGS='-DSW2_SpinupEvaluation -DSW2_PET_Test__petfunc_by_temps -DSW2_SolarPosition_Test__hourangles_by_lat_and_doy -DSW2_SolarPosition_Test__hourangles_by_lats' tidy-test 2>/dev/null
+
+echo $'\n'\
+--------------------------------------------------$'\n'\
+"Tidy text-based SOILWAT2"$'\n'\
+--------------------------------------------------
+
+res=$(make CPPFLAGS='-DSWDEBUG' tidy-bin 2>/dev/null)
+status=$?
+process_clangtidy_results $status "$res"
+
+
+echo $'\n'\
+--------------------------------------------------$'\n'\
+"Tidy nc-based SOILWAT2"$'\n'\
+--------------------------------------------------
+
+res=$(make CPPFLAGS='-DSWNETCDF -DSWUDUNITS -DSWDEBUG' tidy-bin 2>/dev/null)
+status=$?
+process_clangtidy_results $status "$res"
+
+
+echo $'\n'\
+--------------------------------------------------$'\n'\
+"Tidy SOILWAT2 library for rSOILWAT2"$'\n'\
+--------------------------------------------------
+
+res=$(make CPPFLAGS='-DRSOILWAT' CFLAGS='-Iexternal/Rmock' tidy-bin 2>/dev/null)
+status=$?
+process_clangtidy_results $status "$res"
+
+
+echo $'\n'\
+--------------------------------------------------$'\n'\
+"Tidy SOILWAT2 library for STEPWAT2"$'\n'\
+--------------------------------------------------
+
+res=$(make CPPFLAGS='-DSTEPWAT' tidy-bin 2>/dev/null)
+status=$?
+process_clangtidy_results $status "$res"
+
+
+echo $'\n'\
+--------------------------------------------------$'\n'\
+"Tidy SOILWAT2 tests with all extra flags"$'\n'\
+--------------------------------------------------
+
+res=$(make CPPFLAGS='-DSW2_SpinupEvaluation -DSW2_PET_Test__petfunc_by_temps -DSW2_SolarPosition_Test__hourangles_by_lat_and_doy -DSW2_SolarPosition_Test__hourangles_by_lats' tidy-test 2>/dev/null)
+status=$?
+process_clangtidy_results $status "$res"
 
 
 #-------------------------------------------------------------------------------
