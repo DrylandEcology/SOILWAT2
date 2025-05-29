DrylandEcology/SOILWAT2

commit 1bccfb399b8e32806724bafba1d01bd010402225

Fix memory leaks in MPI mode
- There has been 5 memory leaks reported by `make CPPFLAGS=-DSWMPI bin_debug_severe`

- Leak 1 - "txtInFiles"
	* Now call `SW_F_deconstruct()` no matter the process type
	* Let `SW_F_deconstruct()` deallocate what all processes have and ignore things certain processes don't

- Leak 2/3/4 - Weather & SW_RUN_INPUTS
	* `dealloc_IO_info()` now deallocates allocated weather, allocated temporary weather, and array of SW_RUN_INPUTS instances

- Leak 5 - allocated log file name array
	* `logfps` within LOG_INFO is now deallocated within wrapper function `SW_MPI_finalize()`

src/SW_MPI.c
@@ -2649,6 +2649,8 @@ static void alloc_IO_info(
     how many inputs to send to a specific process
 @param[out] tempSWRCPVals A list of lengths that will be used to specify
     how many inputs to send to a specific process
+@param[out] tempWeather A list of temporary weather values, holding more than
+    one site's worth of values
 @param[out] tempSoils A list of lengths that will be used to specify
     how many inputs to send to a specific process
 @param[out] succFlags A list of success flags that are accumulated
@@ -2662,6 +2664,7 @@ static void alloc_IO_info(
 static void dealloc_IO_info(
     size_t numSuids,
     size_t maxNumIOSuids,
+    SW_RUN_INPUTS **runInputs,
     SW_OUT_RUN *tempRun,
     SW_OUT_RUN *OutRun,
     size_t **starts[],
@@ -2674,6 +2677,7 @@ static void dealloc_IO_info(
     double **tempMonthlyVals,
     double **tempSilt,
     double **tempSWRCPVals,
+    double **tempWeather,
     SW_SOIL_RUN_INPUTS **tempSoils,
     Bool **succFlags,
     signed char **succMark,
@@ -2681,8 +2685,8 @@ static void dealloc_IO_info(
 ) {
     int inKey;
     size_t ***deallocStartCount[2] = {NULL};
-    const int numDeallocStartCount = 2;
-    int dealloc;
+    const size_t numDeallocStartCount = 2;
+    size_t dealloc;
     size_t suid;
     void **dealloc1D[] = {
         (void **) sendInputs,
@@ -2691,11 +2695,12 @@ static void dealloc_IO_info(
         (void **) tempMonthlyVals,
         (void **) tempSilt,
         (void **) tempSWRCPVals,
+        (void **) tempWeather,
         (void **) tempSoils,
         (void **) succFlags,
         (void **) succMark
     };
-    const int numDealloc1D = 9;
+    const size_t numDealloc1D = 10;
 
     ForEachNCInKey(inKey) {
         deallocStartCount[0] = &starts[inKey];
@@ -2757,6 +2762,17 @@ static void dealloc_IO_info(
             free(*(dealloc1D[dealloc]));
             *(dealloc1D[dealloc]) = NULL;
         }
+    }
+
+    if (!isnull(*runInputs)) {
+        for (dealloc = 0; dealloc < numSuids; dealloc++) {
+            if (!isnull((*runInputs[dealloc]).weathRunAllHist)) {
+                SW_WTH_deconstruct(&(*runInputs[dealloc]).weathRunAllHist);
+                (*runInputs[dealloc]).weathRunAllHist = NULL;
+            }
+        }
+
+        free((void *) *runInputs);
     }
 
     SW_OUT_deconstruct_outarray(OutRun);
@@ -3586,6 +3602,19 @@ report:
     return failEarly;
 }
 
+/**
+@brief Free allocated log memory in I/O processes
+
+@param[in,out] logs A list of LOG_INFO instances that will be used for
+    getting log information from compute processes
+*/
+static void free_logs(FILE ***logfps) {
+    if (!isnull(*logfps)) {
+        free((void *) *logfps);
+        *logfps = NULL;
+    }
+}
+
 /* =================================================== */
 /*             Global Function Definitions             */
 /* --------------------------------------------------- */
@@ -3652,8 +3681,17 @@ void SW_MPI_initialize(
 /**
 @brief Conclude the program run by finalizing/freeing anything that's
 been initialized/created through MPI within the program run
+
+@param[in] procJob Process job designation used when using MPI
+@param[in,out] LogInfo Holds information on warnings and errors
 */
-void SW_MPI_finalize(void) { MPI_Finalize(); }
+void SW_MPI_finalize(int procJob, LOG_INFO *LogInfo) {
+    if (procJob == SW_MPI_PROC_IO) {
+        free_logs(&LogInfo->logfps);
+    }
+
+    MPI_Finalize();
+}
 
 /**
 @brief Free communicators and types when finishing the program
@@ -6425,6 +6463,7 @@ freeMem:
     dealloc_IO_info(
         numSuidsTot,
         maxSuidsInOutput,
+        &inputs,
         &temp_sw.OutRun,
         &sw->OutRun,
         starts,
@@ -6437,6 +6476,7 @@ freeMem:
         &tempMonthlyVals,
         &tempSilt,
         &tempSoilVals,
+        &tempWeather,
         &tempSoils,
         &succFlags,
         &succMark,

include/SW_MPI.h
@@ -67,7 +67,7 @@ void SW_MPI_initialize(
     MPI_Datatype datatypes[]
 );
 
-void SW_MPI_finalize(void);
+void SW_MPI_finalize(int procJob, LOG_INFO *LogInfo);
 
 void SW_MPI_free_comms_types(
     int rank, SW_MPI_DESIGNATE *desig, MPI_Datatype types[], LOG_INFO *LogInfo

src/SW_Domain.c
@@ -637,18 +637,12 @@ void SW_DOM_deconstruct(SW_DOMAIN *SW_Domain) {
     int k;
     int i;
 
-#if defined(SWMPI)
-    if (SW_Domain->SW_Designation.procJob == SW_MPI_PROC_IO) {
-#endif
-        SW_F_deconstruct(
-            &SW_Domain->SW_PathInputs,
-            SW_Domain->netCDFInput.readInVars,
-            SW_Domain->netCDFInput.useIndexFile,
-            SW_Domain->SW_Designation.procJob
-        );
-#if defined(SWMPI)
-    }
-#endif
+    SW_F_deconstruct(
+        &SW_Domain->SW_PathInputs,
+        SW_Domain->netCDFInput.readInVars,
+        SW_Domain->netCDFInput.useIndexFile,
+        SW_Domain->SW_Designation.procJob
+    );
 
 #if defined(SWNETCDF)
 

src/SW_Main_lib.c
@@ -142,7 +142,7 @@ void sw_init_args(
     int rank,
     Bool *EchoInits,
     char **firstfile,
-    unsigned long *userSUID,
+    size_t *userSUID,
     double *wallTimeLimit,
     Bool *renameDomainTemplateNC,
     Bool *prepareFiles,
@@ -693,6 +693,8 @@ void sw_finalize_program(
     LOG_INFO *LogInfo
 ) {
 #if defined(SWMPI)
+    int procJob = SW_Domain->SW_Designation.procJob;
+
     /* Report information for the following scenarios
         1) Failed during setup - error(s)/warning(s)
         2) Failed during simulation runs - simulations statistics *
@@ -728,6 +730,6 @@ void sw_finalize_program(
     sw_wrapup_logs(rank, LogInfo);
 
 #if defined(SWMPI)
-    SW_MPI_finalize();
+    SW_MPI_finalize(procJob, LogInfo);
 #endif
 }

commit 2db4a278991e139c147988f65de42fc3ec8d13b1

Properly deallocate input memory
- Commit 1bccfb39 introduced the deallocation of memory that was discovered to be causing leaks on Linux
- This introduced a segmentation fault when trying to deallocate more than 1 weather input
	* This was experienced when running 5 processes on an HPC Linux environment

- Adjust the parentheses when deallocating `runInputs` in `dealloc_IO_info()`
- Set `runInputs` to NULL after deallocation (not part of the segmentation fault)

- Program was able to successfully finish on HPC environment

src/SW_MPI.c
@@ -2754,13 +2754,14 @@ static void dealloc_IO_info(
 
     if (!isnull(*runInputs)) {
         for (dealloc = 0; dealloc < numSuids; dealloc++) {
-            if (!isnull((*runInputs[dealloc]).weathRunAllHist)) {
-                SW_WTH_deconstruct(&(*runInputs[dealloc]).weathRunAllHist);
-                (*runInputs[dealloc]).weathRunAllHist = NULL;
+            if (!isnull((*runInputs)[dealloc].weathRunAllHist)) {
+                SW_WTH_deconstruct(&(*runInputs)[dealloc].weathRunAllHist);
+                (*runInputs)[dealloc].weathRunAllHist = NULL;
             }
         }
 
         free((void *) *runInputs);
+        *runInputs = NULL;
     }
 
     SW_OUT_deconstruct_outarray(OutRun);