d7243c845749074af60f7636408476eba43a82ff
Daniel Schlaepfer <danielrschlaepfer@gmail.com>
2025-09-05 11:22:09 -0400

    Improve template project script "collapseLogfiles.sh"
    - if the concatenation was successful, then remove the individual IO and COMP logfiles

--- templateProject/scripts/collapseLogfiles.sh
+++ templateProject/scripts/collapseLogfiles.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-find ../logs/ -maxdepth 1 \( -name "*logfile.*" \) -print0 |
+pathLog="../logs"
+patternLog="*logfile.*"
+newLogfile="${pathLog}/logfile-combined.txt"
+
+
+# Concatenate individual logfiles from IO- and COMP-processes into one logfile
+find "${pathLog}" -maxdepth 1 \( -name "${patternLog}" \) -print0 |
     sort -zV |
     while IFS= read -r -d '' f; do
         echo "====== ${f#./} ======"
         cat "$f"
         echo
-    done > ../logs/logfile-combined.txt
+    done > "${newLogfile}"
 
+
+# Remove individual logfiles if concatenation was successful
+if [[ "${PIPESTATUS[@]}" =~ [^0\ ] ]]; then
+    echo "collapseLogfiles.sh encountered errors."
+
+elif [ -f "${newLogfile}" ]; then
+    find "${pathLog}" -type f \( -name "*_IO_logfile.txt" -or -name "*_COMP_logfile.txt" \) -delete
+fi
