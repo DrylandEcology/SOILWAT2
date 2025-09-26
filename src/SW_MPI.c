DrylandEcology/SOILWAT2

commit b59eac8f00ea35c82ad33e1e6c3e3be800540d1a

Initialize netCDF `counts` `lonIndex`to 0
- Currently, when calculating contiguous sites in `get_contiguous_counts()`, we initialize the longitude value of `counts` to 1 if the domain is site-based

- Set longitude to 0 instead of 1 in `counts` array

src/SW_MPI.c
@@ -510,7 +510,7 @@ static void get_contiguous_counts(
     starts[0][1] = prevX;
 
     counts[0][0] = (sDom) ? numDomSuids : 1;
-    counts[0][1] = (sDom) ? 1 : N_SUID_ASSIGN; // NOLINT(bugprone-branch-clone)
+    counts[0][1] = (sDom) ? 0 : N_SUID_ASSIGN; // NOLINT(bugprone-branch-clone)
 
     if (useSuccFlags) {
         currFlag = succFlags[0];

commit f844f613b53ae62b05fed6ce4abeee003cc5d57e

Properly set longitude value in `count` when calculating contiguous sites
- Commit b59eac8f00ea35c82ad33e1e6c3e3be800540d1a introduced a bug when setting the longitude value in `count` arrays
- The change in this commit, however is not incorrect
- The counts are not being calculated when domains are not the same between the program's domain and the input's

- When calculating all keys contiguous sites, now if the domain types are not the same, calculate the starts/counts
	* Even if no index file is used

src/SW_MPI.c
@@ -617,13 +617,15 @@ static void calculate_contiguous_allkeys(
     int inKey;
     Bool useIndex;
     Bool useTranslated;
+    Bool sameDomType;
     size_t suid;
 
     ForEachNCInKey(inKey) {
         useIndex = (inKey != eSW_InDomain) ? useIndexFile[inKey] : swFALSE;
+        sameDomType = (Bool) (sDoms[inKey] == sDoms[eSW_InDomain]);
 
         if (inKey == eSW_InDomain || readInVars[inKey][0]) {
-            if (inKey == eSW_InDomain || useIndex) {
+            if (inKey == eSW_InDomain || useIndex || !sameDomType) {
                 useTranslated = (Bool) (inKey != eSW_InDomain && useIndex);
 
                 get_contiguous_counts(