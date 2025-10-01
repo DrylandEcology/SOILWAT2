cf26822d6db5b5ca3d6969a5b37bef49f56b5fb7
Daniel Schlaepfer <danielrschlaepfer@gmail.com>
2025-10-01 17:38:57 -0400

    Improved test and debug scripts
    - compiler flags and warnings are now provided by dedicated functions
    - several new flags added based on recommendations from https://best.openssf.org/Compiler-Hardening-Guides/Compiler-Options-Hardening-Guide-for-C-and-C++.html
    - notably, -Wformat=2 and -Wconversion are not included but should be considered for the future

---
+++ tools/compile_flags.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+# $1 optimization level: O0, O1
+debug_flags() {
+    local opt="$1"
+    local res=""
+
+    case $opt in
+        O0) res="-g -O0 -DSWDEBUG" ;;
+        O1) res="-g -O1 -DSWDEBUG" ;;
+
+        *) res="-g -$opt -DSWDEBUG" ;;
+    esac
+
+    echo "$res"
+}
+
+
+# $1 language: "CC" or "CXX"
+# $2 sanitizer: "", "sanitizer-no" or "sanitizer-yes"
+#   optimization level of 1 is recommended for -fsanitize=undefined
+compile_flags() {
+    local res=""
+    local lang="$1"
+    local useSanitizer="$2"
+
+    #--- List flags
+    # see also recommendations by
+    # https://best.openssf.org/Compiler-Hardening-Guides/Compiler-Options-Hardening-Guide-for-C-and-C++.html
+
+    # TODO: address underlying problems so that we can add -Wformat=2
+    # TODO: address underlying problems so that we can add -Wconversion
+    local flags="\
+        -Wall \
+        -Wextra \
+        -Wpedantic \
+        -Werror \
+        -Werror=implicit \
+        -Werror=incompatible-pointer-types \
+        -Werror=int-conversion \
+        -Wcast-align \
+        -Wdeprecated \
+        -Wformat \
+        -Wimplicit-fallthrough \
+        -Wmissing-declarations \
+        -Wnull-dereference \
+        -Wredundant-decls \
+        -Wshadow \
+        -U_FORTIFY_SOURCE \
+        -D_FORTIFY_SOURCE=3 \
+        -D_GLIBCXX_ASSERTIONS \
+        -fno-delete-null-pointer-checks \
+        -fno-strict-overflow \
+        -fno-strict-aliasing \
+        -ftrivial-auto-var-init=zero \
+        -fstack-protector-all \
+        -fstack-protector-strong \
+        -fstrict-aliasing \
+        -fstrict-flex-arrays=3
+        -fno-omit-frame-pointer \
+        -fno-common \
+        "
+
+
+    # '-Wstrict-prototypes' is valid for C/ObjC but not for C++
+    local flags_cc="-Wstrict-prototypes"
+
+    # TODO: address underlying problems so that we can eliminate
+    # `-Wno-error=deprecated`
+    # (https://github.com/DrylandEcology/SOILWAT2/issues/208):
+    # treating 'c' input as 'c++' when in C++ mode, this behavior is deprecated
+    local flags_cxx="-Wno-error=deprecated"
+
+    local flags_sanitizer="\
+        -fsanitize=undefined \
+        -fsanitize=address \
+        "
+
+
+    #--- Put flags together for requested compilation
+    res="$flags"
+
+    case $lang in
+        CC|cc) res="$res $flags_cc" ;;
+        CXX|cxx) res="$res $flags_cxx" ;;
+
+        *) echo "$lang"" is not implemented."; exit 1 ;;
+    esac
+
+    if test -z "$useSanitizer"; then
+        useSanitizer="sanitizer-no"
+    fi
+
+    case $useSanitizer in
+        sanitizer-no) ;;
+        sanitizer-yes) res="$res $flags_sanitizer" ;;
+
+        *) echo "$useSanitizer"" is not implemented."; exit 1 ;;
+    esac
+
+    echo $res
+}
