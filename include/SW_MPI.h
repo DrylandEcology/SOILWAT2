263ac6c8e9547158316d4aa92ed05aa84a39f387
Nicholas Persley <nsp73@nau.edu>
2025-12-05 15:37:24 -0500

    Remove unused functions
    - Remove functions that have become obsolete due to the changes in the space-before-time commits
    
    - Remove `SW_CTL_run_sw`
    - Remove the following MPI-related functions (with any helpers)
    	* SW_MPI_setup_inputs, SW_MPI_get_sim_suids
    	* SW_MPI_write_outputs, SW_MPI_store_outputs
    	* SW_MPI_Scatter

--- include/SW_MPI.h
+++ include/SW_MPI.h
@@ -63,19 +63,6 @@ void SW_MPI_Allreduce(
     MPI_Comm comm
 );
 
-void SW_MPI_Scatter(
-    MPI_Comm comm,
-    void *buffer,
-    int *sendCount,
-    MPI_Datatype sendType,
-    int recvCount,
-    MPI_Datatype recvType,
-    int src,
-    Bool vectorized,
-    int **displacements,
-    void *dest
-);
-
 void SW_MPI_Barrier(MPI_Comm comm);
 
 void SW_MPI_Bcast(
@@ -90,56 +77,6 @@ void SW_MPI_get_end_info(
     size_t nActiveSites,
     SW_WALLTIME *SW_WallTime,
     LOG_INFO *LogInfo
-);
-
-void SW_MPI_store_outputs(
-    size_t runNum,
-    SW_OUT_DOM *OutDom,
-    double *src_p_OUT[][SW_OUTNPERIODS],
-    double *dest_p_OUT[][SW_OUTNPERIODS]
-);
-
-void SW_MPI_get_sim_suids(
-    Bool *readInVars[],
-    size_t *domSuids[],
-    const Bool useIndexFile[],
-    size_t *readIndex,
-    unsigned int *nSuidsLeft,
-    size_t simSuids[SW_NINKEYSNC][N_SUID_ASSIGN][2],
-    unsigned int *nSuids
-);
-
-void SW_MPI_write_outputs(
-    SW_PATH_OUTPUTS *SW_PathOutputs,
-    int progFileID,
-    int progVarID,
-    double *main_p_OUT[][SW_OUTNPERIODS],
-    double *temp_p_OUT[][SW_OUTNPERIODS],
-    size_t distSUIDs[][2],
-    size_t numSuids,
-    Bool siteDom,
-    SW_OUT_DOM *OutDom,
-    Bool succFlags[],
-    size_t starts[][2],
-    size_t counts[][2],
-    SW_WALLTIME *SW_WallTime,
-    LOG_INFO *LogInfo
-);
-
-void SW_MPI_setup_inputs(
-    SW_RUN *sw_template,
-    SW_RUN_INPUTS *runInputs,
-    SW_OUT_DOM *OutDom,
-    int numCyclesProc,
-    Bool readWeather,
-    IntU n_years,
-    SW_OUT_RUN *tempOut,
-    Bool *extraFailCheck,
-    LOG_INFO *LogInfo
-);
-
-void SW_MPI_dealloc_inputs(
-    SW_RUN_INPUTS runInputs[], SW_OUT_RUN *OutRun, SW_OUT_RUN *tempOut
 );
 
 #ifdef __cplusplus
