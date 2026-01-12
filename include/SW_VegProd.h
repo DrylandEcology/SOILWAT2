e2cfd3e4eadda10e3b7b7ccd8a09876c4915c2a4
Daniel Schlaepfer <danielrschlaepfer@gmail.com>
2026-01-12 17:22:31 -0500

    Land cover normalization
    - fixVegCoverInputs(), renamed from SW_VPD_fix_cover(), is applied to txt-based (as previously) and now also to nc-based inputs via SW_VPD_init_run() -- except if vegetation is estimated from long-term dynamics
    - fixVegCoverInputs() now normalizes fractional land cover to sum exactly to 1 (previously, land cover was only normalized if total cover deviated from 1 by more than 1e-4)

--- include/SW_VegProd.h
+++ include/SW_VegProd.h
@@ -197,7 +197,7 @@ void SW_VPD_new_year(
     LOG_INFO *LogInfo
 );
 
-void SW_VPD_fix_cover(
+void fixVegCoverInputs(
     SW_VEGPROD_RUN_INPUTS *SW_VegProdRunIn, LOG_INFO *LogInfo
 );
 
--- src/SW_VegProd.c
+++ src/SW_VegProd.c
@@ -1730,60 +1730,87 @@ void SW_VPD_read(
         );
     }
 
-    SW_VPD_fix_cover(SW_VegProdRunIn, LogInfo);
-
 closeFile: { CloseFile(&f, LogInfo); }
 }
 
 /**
-@brief Check that sum of land cover is 1; adjust if not.
+@brief Normalize fractional cover inputs
+
+Fractional cover of bare ground and of each vegetation type are normalized
+so that the sum across all cover components is exactly 1 (full land cover).
+
+Warn user if the sum deviates from 1 by more than a tolerance of 1e-4.
 
 @param[in,out] SW_VegProdRunIn Struct of type SW_VEGPROD_RUN_INPUTS that
     holds run-specific input information about vegetation production
 @param[out] LogInfo Holds information on warnings and errors
 
 @sideeffect
-- Adjusts `SW_VegProdRunIn->bare_cov.fCover` and
-`SW_VegProdRunIn->veg[k].cov.fCover` to sum to 1.
-- Print a warning that values are adjusted and notes with the new values.
+- Adjusted `SW_VegProdRunIn->bare_cov.fCover` and
+`SW_VegProdRunIn->veg[k].cov.fCover`
 */
-void SW_VPD_fix_cover(
+void fixVegCoverInputs(
     SW_VEGPROD_RUN_INPUTS *SW_VegProdRunIn, LOG_INFO *LogInfo
 ) {
     int k;
     double fraction_sum = 0.;
+    double tolerance = 1e-4; // txt-input precision may be at most 3-4 digits
+    char msg[MAX_LOG_SIZE] = {'\0'};
+    char buf[MAX_LOG_SIZE] = {'\0'};
+    char *writePtr = NULL;
+    char *writeEndPtr = NULL;
+    size_t writeSize;
+    int bufSize;
+    Bool fullBuffer = swFALSE;
+    int countFullBuffer = 0;
 
+
+    /* Normalize cover */
     fraction_sum = SW_VegProdRunIn->bare_cov.fCover;
     ForEachVegType(k) { fraction_sum += SW_VegProdRunIn->veg[k].cov.fCover; }
 
-    if (!EQ_w_tol(fraction_sum, 1.0, 1e-4)) {
-        // inputs are never more precise than at most 3-4 digits
+    SW_VegProdRunIn->bare_cov.fCover /= fraction_sum;
+    ForEachVegType(k) { SW_VegProdRunIn->veg[k].cov.fCover /= fraction_sum; }
 
-        LogError(
-            LogInfo,
-            LOGWARN,
-            "Fractions of land cover components were normalized: "
-            "Sum of fractions was %.4f instead of 1.0. New coefficients are: ",
-            fraction_sum
-        );
 
-        SW_VegProdRunIn->bare_cov.fCover /= fraction_sum;
-        LogError(
-            LogInfo,
-            LOGWARN,
-            "Bare ground fraction = %.4f",
+    /* Generate warning if adjustment > tolerance */
+    if (!EQ_w_tol(fraction_sum, 1.0, tolerance)) {
+        writePtr = msg;
+        writeEndPtr = msg + sizeof msg - 1;
+        writeSize = MAX_LOG_SIZE;
+
+        bufSize = snprintf(
+            buf,
+            sizeof buf,
+            "Normalized fractional land cover inputs: "
+            "previous sum = %.6f instead of 1.0. "
+            "Updated cover: bare ground = %.4f",
+            fraction_sum,
             SW_VegProdRunIn->bare_cov.fCover
         );
+        fullBuffer = sw_memccpy_inc(
+            (void **) &writePtr, writeEndPtr, (void *) buf, '\0', &writeSize
+        );
+        countFullBuffer += (fullBuffer || bufSize >= MAX_LOG_SIZE) ? 1 : 0;
 
         ForEachVegType(k) {
-            SW_VegProdRunIn->veg[k].cov.fCover /= fraction_sum;
-            LogError(
-                LogInfo,
-                LOGWARN,
-                "%s fraction = %.4f",
+            bufSize = snprintf(
+                buf,
+                sizeof buf,
+                ", %s = %.4f",
                 key2veg[k],
                 SW_VegProdRunIn->veg[k].cov.fCover
             );
+            fullBuffer = sw_memccpy_inc(
+                (void **) &writePtr, writeEndPtr, (void *) buf, '\0', &writeSize
+            );
+            countFullBuffer += (fullBuffer || bufSize >= MAX_LOG_SIZE) ? 1 : 0;
+        }
+
+        LogError(LogInfo, LOGWARN, "%s", msg);
+
+        if (countFullBuffer > 0) {
+            reportFullBuffer(LOGERROR, LogInfo);
         }
     }
 }
@@ -1959,6 +1986,7 @@ void SW_VPD_init_run(SW_RUN *sw, LOG_INFO *LogInfo) {
     }
 
     if (veg_method != VEG_METHOD_DYN_EST) {
+        fixVegCoverInputs(&sw->RunIn.VegProdRunIn, LogInfo);
         checkVegetation(&sw->RunIn.VegProdRunIn, LogInfo);
     }
 }
@@ -1984,7 +2012,7 @@ void checkVegetation(
         LogError(
             LogInfo,
             LOGERROR,
-            "bare-ground cover (%.4f) is outside 0-1",
+            "bare-ground cover (%.6f) is outside 0-1",
             totalCover
         );
         return;
@@ -2069,7 +2097,7 @@ void checkVegetation(
         LogError(
             LogInfo,
             LOGERROR,
-            "sum of cover components (%.4f) is outside 0-1",
+            "sum of cover components (%.6f) is outside 0-1",
             totalCover
         );
     }
