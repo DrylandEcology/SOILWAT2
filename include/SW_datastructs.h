03a6cc9bd1ebd9358467da8a1008bc4ffd989925
Nicholas Persley <nsp73@nau.edu>
2026-09-17 09:25:56 -0400

    Fix memory leaks
    - Update `SW_NOUTFILES` from 8 --> 10 to deallocate all text output file name memory

--- include/SW_datastructs.h
+++ include/SW_datastructs.h
@@ -32,7 +32,7 @@
 #endif
 
 #define SW_NINFILES 21                       // For input `txtInFiles`
-#define SW_NOUTFILES 8                       // For output `txtInFiles`
+#define SW_NOUTFILES 10                      // For output `txtInFiles`
 #define SW_NFILES SW_NINFILES + SW_NOUTFILES // For `txtInFiles`
 #define SW_NVARDOM 3                         // For `InFilesNC`
 /** Maximum number of variables (columns) per output group */
